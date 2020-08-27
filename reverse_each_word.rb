949fbf302c37b79562c11981b18e782bd2e0c084
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-27 01:25:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
+  sentence_array = sentence.split
   sentence_array.collect { |word| word.reverse }.join(" ")
 end
