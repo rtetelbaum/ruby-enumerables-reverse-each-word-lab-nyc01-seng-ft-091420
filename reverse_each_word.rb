dc563790777c9450c25e3af28382ffea5ec38db2
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-27 01:35:01 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,9 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
   reverse_sentence_array = []
-  sentence_array.each { |word| reverse_sentence_array << word.reverse }
+  sentence.split.each { |word| reverse_sentence_array << word.reverse }
   reverse_sentence_array.join(" ")
 end
 
-
-
-
-
 def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+  sentence.split.collect { |word| word.reverse }.join(" ")
 end
-
